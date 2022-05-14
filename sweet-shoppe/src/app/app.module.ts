32a29a6f9f9b484439d50c984eee181be71e95b9
Jordan <jbrockerrudow@gmail.com>
2022-05-14 10:31:44 -0500

    Dependencies added & Angular Material integrated

--- sweet-shoppe/src/app/app.module.ts
+++ sweet-shoppe/src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import hammerjs;
 
+// Angular Material
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+// App Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { ProductComponent } from './product/product.component';
-import hammerjs;
+
 
 @NgModule({
   declarations: [
@@ -15,7 +25,13 @@ import hammerjs;
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
