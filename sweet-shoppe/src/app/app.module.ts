f94bf101ca5fb52da53c9ea7820ee00aeee67b4c
Jordan <jbrockerrudow@gmail.com>
2022-05-14 13:21:27 -0500

    Bug fixes - imports

--- sweet-shoppe/src/app/app.module.ts
+++ sweet-shoppe/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import hammerjs;
+import 'hammerjs';
 
 // Angular Material
 import {MatToolbarModule} from '@angular/material/toolbar';
