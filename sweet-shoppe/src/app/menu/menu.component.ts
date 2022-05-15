5a9ed802a7ca9df154e1d1eab01187b406ef9b2d
Jordan <jbrockerrudow@gmail.com>
2022-05-15 06:07:55 -0500

    Mock product data integrated

--- sweet-shoppe/src/app/menu/menu.component.ts
+++ sweet-shoppe/src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../shared/product';
 
 
 @Component({
@@ -7,6 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+
+  products: Product[] = [
+    {
+      id: '0',
+      name: 'Sour Patch Kids',
+      image: '/assets/images/product/sourpatchkids.png',
+      category: 'sour-candy',
+      featured: false,
+      label: 'sour',
+      price: '6.99',
+      // tslint:disable-next-line:max-line-length
+      description: "Sour Patch Kids - First they're sour. Then they're sweet. Sour Patch Kids are a fun, soft, and chewy candy for children and adults. They are sweet and chewy and packed with Sour Patch pucker power. Keep everyone satisfied when you bring all your favorite Kids to the party.",
+    },
+    {
+      id: '1',
+      name: '49 Assorted Jelly Bean Flavors',
+      image: '/assets/images/product/jellybelly49assorted.jpeg',
+      category: 'fruity-candy',
+      featured: false,
+      label: 'fruity',
+      price: '11.49',
+      // tslint:disable-next-line:max-line-length
+      description: "All Jelly Belly beans are OU Kosher, gluten free, peanut free, dairy free, fat free, and vegetarian friendly. Our beans have a 24-month shelf life.  Sold by weight, not volume. At Jelly Belly, your satisfaction is our greatest concern. Jelly Belly candies are made with the finest ingredients, using natural ingredients whenever possible.",
+    }
+  ];
+
 
   constructor() { }
 
