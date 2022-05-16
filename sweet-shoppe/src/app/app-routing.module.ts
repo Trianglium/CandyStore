b3819cab3cc9de705b9f29303c53b029894e0570
Jordan <jbrockerrudow@gmail.com>
2022-05-16 10:25:06 -0500

    Configure routing and integrate into main module

--- sweet-shoppe/src/app/app-routing.module.ts
+++ sweet-shoppe/src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { routes } from './shared/routes';
 
-import { routes } from '../routes';
 
-const routes: Routes = [];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule, RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule],
   declarations: []
 })
--- sweet-shoppe/src/app/app.module.ts
+++ sweet-shoppe/src/app/app.module.ts
@@ -4,6 +4,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import 'hammerjs';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 // Angular Material
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -16,10 +17,9 @@ import {MatCardModule} from '@angular/material/card';
 // App Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 import { MenuComponent } from './menu/menu.component';
 import { ProductComponent } from './product/product.component';
-import { FooterComponent } from './footer/footer.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
@@ -37,6 +37,7 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserAnimationsModule,
+    AppRoutingModule,
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
