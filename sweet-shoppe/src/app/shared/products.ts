6856d932701e56157ef57bae42f0c50c25f292ec
Jordan <jbrockerrudow@gmail.com>
2022-05-16 10:30:00 -0500

    Modularize Product Component and data

---
+++ sweet-shoppe/src/app/shared/products.ts
@@ -0,0 +1,93 @@
+import { Product } from './product';
+export const PRODUCTS: Product[] = [
+  {
+    id: '0',
+    name: 'Sour Patch Kids',
+    image: '/assets/images/product/sourpatchkids.png',
+    category: 'candy',
+    featured: false,
+    price: '6.99',
+    // tslint:disable-next-line:max-line-length
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elit massa, facilisis id sem a, tincidunt condimentum sem. Mauris a nulla est. Nulla commodo eget arcu et vehicula. Etiam rhoncus vitae tortor nec sodales.',
+  },
+  {
+    id: '1',
+    name: '49 Assorted Jelly Bean Flavors',
+    image: '/assets/images/product/jellybelly49assorted.jpeg',
+    category: 'candy',
+    featured: false,
+    price: '11.49',
+    // tslint:disable-next-line:max-line-length
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elit massa, facilisis id sem a, tincidunt condimentum sem. Mauris a nulla est. Nulla commodo eget arcu et vehicula. Etiam rhoncus vitae tortor nec sodales.',
+  },
+  {
+    id: '2',
+    name: 'Strawberry Yogurt Parfait',
+    image: '/assets/images/product/fruitparfaitstrawberry.png',
+    category: 'treats',
+    featured: false,
+    price: '3.97',
+    // tslint:disable-next-line:max-line-length
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elit massa, facilisis id sem a, tincidunt condimentum sem. Mauris a nulla est. Nulla commodo eget arcu et vehicula. Etiam rhoncus vitae tortor nec sodales.',
+  },
+  {
+    id: '3',
+    name: 'Chocolate Covered Strawberries',
+    image: '/assets/images/product/chocstrawberries.png',
+    category: 'treats',
+    featured: false,
+    price: '14.99',
+    // tslint:disable-next-line:max-line-length
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elit massa, facilisis id sem a, tincidunt condimentum sem. Mauris a nulla est. Nulla commodo eget arcu et vehicula. Etiam rhoncus vitae tortor nec sodales.',
+  },
+  {
+    id: '4',
+    name: 'Ice Cream Cup',
+    image: '/assets/images/product/icecream.png',
+    category: 'treats',
+    featured: false,
+    price: '4.99',
+    // tslint:disable-next-line:max-line-length
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elit massa, facilisis id sem a, tincidunt condimentum sem. Mauris a nulla est. Nulla commodo eget arcu et vehicula. Etiam rhoncus vitae tortor nec sodales.',
+  },
+  {
+    id: '5',
+    name: 'Candied Apples',
+    image: '/assets/images/product/candyapples.png',
+    category: 'treats',
+    featured: false,
+    price: '3.99',
+    // tslint:disable-next-line:max-line-length
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elit massa, facilisis id sem a, tincidunt condimentum sem. Mauris a nulla est. Nulla commodo eget arcu et vehicula. Etiam rhoncus vitae tortor nec sodales.',
+  },
+  {
+    id: '6',
+    name: 'Funfetti Cookie Sandiwch',
+    image: '/assets/images/product/funfetti-cookie-sandiwches.png',
+    category: 'treats',
+    featured: false,
+    price: '4.99',
+    // tslint:disable-next-line:max-line-length
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elit massa, facilisis id sem a, tincidunt condimentum sem. Mauris a nulla est. Nulla commodo eget arcu et vehicula. Etiam rhoncus vitae tortor nec sodales.',
+  },
+  {
+    id: '7',
+    name: 'Galaxy Cupcakes',
+    image: '/assets/images/product/Galaxy-Cupcakes.png',
+    category: 'treats',
+    featured: false,
+    price: '7.49',
+    // tslint:disable-next-line:max-line-length
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elit massa, facilisis id sem a, tincidunt condimentum sem. Mauris a nulla est. Nulla commodo eget arcu et vehicula. Etiam rhoncus vitae tortor nec sodales.',
+  },
+  {
+    id: '8',
+    name: 'Red Velvet Cookie Sandwich',
+    image: '/assets/images/product/red-velvet-sandwich-cookie.png',
+    category: 'treats',
+    featured: false,
+    price: '3.99',
+    // tslint:disable-next-line:max-line-length
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elit massa, facilisis id sem a, tincidunt condimentum sem. Mauris a nulla est. Nulla commodo eget arcu et vehicula. Etiam rhoncus vitae tortor nec sodales.',
+  }
+];
