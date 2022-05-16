347b6b49a4cd81cec721f0fdd8e33de33da9c6f7
Jordan <jbrockerrudow@gmail.com>
2022-05-16 06:05:18 -0500

    Routing initial setup

--- sweet-shoppe/src/app/app-routing.module.ts
+++ sweet-shoppe/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+
+import { routes } from '../routes';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  declarations: []
 })
 export class AppRoutingModule { }
