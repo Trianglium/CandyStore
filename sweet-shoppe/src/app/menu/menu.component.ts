8c969cd9efe0ddfa388aa87a42f0e2119f6d98fd
Jordan <jbrockerrudow@gmail.com>
2022-05-16 10:10:11 -0500

    Update data in Menu Component

--- sweet-shoppe/src/app/menu/menu.component.ts
+++ sweet-shoppe/src/app/menu/menu.component.ts
@@ -14,45 +14,91 @@ export class MenuComponent implements OnInit {
       id: '0',
       name: 'Sour Patch Kids',
       image: '/assets/images/product/sourpatchkids.png',
-      category: 'sour-candy',
+      category: 'candy',
       featured: false,
-      label: 'sour',
       price: '6.99',
       // tslint:disable-next-line:max-line-length
-      description: "Sour Patch Kids - First they're sour. Then they're sweet. Sour Patch Kids are a fun, soft, and chewy candy for children and adults. They are sweet and chewy and packed with Sour Patch pucker power. Keep everyone satisfied when you bring all your favorite Kids to the party.",
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elit massa, facilisis id sem a, tincidunt condimentum sem. Mauris a nulla est. Nulla commodo eget arcu et vehicula. Etiam rhoncus vitae tortor nec sodales.',
     },
     {
       id: '1',
       name: '49 Assorted Jelly Bean Flavors',
       image: '/assets/images/product/jellybelly49assorted.jpeg',
-      category: 'fruity-candy',
+      category: 'candy',
       featured: false,
-      label: 'fruity',
       price: '11.49',
       // tslint:disable-next-line:max-line-length
-      description: "All Jelly Belly beans are OU Kosher, gluten free, peanut free, dairy free, fat free, and vegetarian friendly. Our beans have a 24-month shelf life.  Sold by weight, not volume. At Jelly Belly, your satisfaction is our greatest concern. Jelly Belly candies are made with the finest ingredients, using natural ingredients whenever possible.",
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elit massa, facilisis id sem a, tincidunt condimentum sem. Mauris a nulla est. Nulla commodo eget arcu et vehicula. Etiam rhoncus vitae tortor nec sodales.',
     },
     {
-      "id": "2",
-      "name": "Strawberry Yogurt Parfait",
-      "image": "/assets/images/product/fruitparfaitstrawberry.png",
-      "category": "treats",
-      "featured": false,
-      "label": "fruity",
-      "price": "3.97",
+      id: '2',
+      name: 'Strawberry Yogurt Parfait',
+      image: '/assets/images/product/fruitparfaitstrawberry.png',
+      category: 'treats',
+      featured: false,
+      price: '3.97',
       // tslint:disable-next-line:max-line-length
-      "description": "The Marketside Yogurt Parfait is available in a tasty 8.5-oz container and has all the ingredients you need to fill up on healthy goodness. It contains vanilla yogurt, fruit and crunchy granola, giving you sweet flavor with a bit of crunch to satisfy your craving. This yogurt granola parfait is an ideal breakfast that you can eat at home or while you are on the go. Each serving is only 70 calories and contains vital nutrients such as fiber, vitamin A, vitamin C, calcium and iron."
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elit massa, facilisis id sem a, tincidunt condimentum sem. Mauris a nulla est. Nulla commodo eget arcu et vehicula. Etiam rhoncus vitae tortor nec sodales.',
     },
     {
-      "id": "3",
-      "name": "Chocolate Covered Strawberries",
-      "image": "/assets/images/product/chocstrawberries.jpg",
-      "category": "treats",
-      "featured": false,
-      "label": "chocolate",
-      "price": "3.97",
+      id: '3',
+      name: 'Chocolate Covered Strawberries',
+      image: '/assets/images/product/chocstrawberries.png',
+      category: 'treats',
+      featured: false,
+      price: '14.99',
       // tslint:disable-next-line:max-line-length
-      "description": "Milk Chocolate covered, organic strabwerries. Choice of chocolate design."
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elit massa, facilisis id sem a, tincidunt condimentum sem. Mauris a nulla est. Nulla commodo eget arcu et vehicula. Etiam rhoncus vitae tortor nec sodales.',
+    },
+    {
+      id: '4',
+      name: 'Ice Cream Cup',
+      image: '/assets/images/product/icecream.png',
+      category: 'treats',
+      featured: false,
+      price: '4.99',
+      // tslint:disable-next-line:max-line-length
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elit massa, facilisis id sem a, tincidunt condimentum sem. Mauris a nulla est. Nulla commodo eget arcu et vehicula. Etiam rhoncus vitae tortor nec sodales.',
+    },
+    {
+      id: '5',
+      name: 'Candied Apples',
+      image: '/assets/images/product/candyapples.png',
+      category: 'treats',
+      featured: false,
+      price: '3.99',
+      // tslint:disable-next-line:max-line-length
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elit massa, facilisis id sem a, tincidunt condimentum sem. Mauris a nulla est. Nulla commodo eget arcu et vehicula. Etiam rhoncus vitae tortor nec sodales.',
+    },
+    {
+      id: '6',
+      name: 'Funfetti Cookie Sandiwch',
+      image: '/assets/images/product/funfetti-cookie-sandiwches.png',
+      category: 'treats',
+      featured: false,
+      price: '4.99',
+      // tslint:disable-next-line:max-line-length
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elit massa, facilisis id sem a, tincidunt condimentum sem. Mauris a nulla est. Nulla commodo eget arcu et vehicula. Etiam rhoncus vitae tortor nec sodales.',
+    },
+    {
+      id: '7',
+      name: 'Galaxy Cupcakes',
+      image: '/assets/images/product/Galaxy-Cupcakes.png',
+      category: 'treats',
+      featured: false,
+      price: '7.49',
+      // tslint:disable-next-line:max-line-length
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elit massa, facilisis id sem a, tincidunt condimentum sem. Mauris a nulla est. Nulla commodo eget arcu et vehicula. Etiam rhoncus vitae tortor nec sodales.',
+    },
+    {
+      id: '8',
+      name: 'Red Velvet Cookie Sandwich',
+      image: '/assets/images/product/red-velvet-sandwich-cookie.png',
+      category: 'treats',
+      featured: false,
+      price: '3.99',
+      // tslint:disable-next-line:max-line-length
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elit massa, facilisis id sem a, tincidunt condimentum sem. Mauris a nulla est. Nulla commodo eget arcu et vehicula. Etiam rhoncus vitae tortor nec sodales.',
     }
   ];
 
