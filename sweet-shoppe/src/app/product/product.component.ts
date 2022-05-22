6633c1adea33edd54b2139c50ca99f9e86d97e71
Jordan <jbrockerrudow@gmail.com>
2022-05-22 11:22:27 -0500

    Temporarily remove productservice

--- sweet-shoppe/src/app/product/product.component.ts
+++ sweet-shoppe/src/app/product/product.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { Product } from '../shared/product';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -11,9 +14,13 @@ export class ProductComponent implements OnInit {
   @Input()
   product: Product;
 
-  constructor() { }
+  constructor(private productservice: ProductService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.params['id'];
+    //this.product = this.productservice.getProduct(id);
   }
 
 }
