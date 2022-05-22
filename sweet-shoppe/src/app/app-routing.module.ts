7b3c90424a29028a747d912351f519d2d0633f1f
Jordan <jbrockerrudow@gmail.com>
2022-05-22 12:14:57 -0500

    Routing to all main components

--- sweet-shoppe/src/app/app-routing.module.ts
+++ sweet-shoppe/src/app/app-routing.module.ts
@@ -2,11 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { routes } from './shared/routes';
+import { MenuComponent } from './menu/menu.component';
+import { ProductComponent } from './product/product.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
   },
   {
     path: 'menu', component: MenuComponent,
