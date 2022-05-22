c9751d7b29db3fc55b0e5f54a2965a1d747ea342
Jordan <jbrockerrudow@gmail.com>
2022-05-22 11:23:58 -0500

    Initialize services directory & Implement product service

---
+++ sweet-shoppe/src/app/services/product.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../shared/product';
+import { PRODUCTS } from '../shared/products';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor() { }
+
+  getProducts(): Product[] {
+    return PRODUCTS;
+  }
+}
