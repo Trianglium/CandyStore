8835fea5259d2834f3cca35c8e9b76c6a21a7c09
Jordan <jbrockerrudow@gmail.com>
2022-05-23 08:35:18 -0500

    Named contact type variables integrated

--- sweet-shoppe/src/app/contact/contact.component.ts
+++ sweet-shoppe/src/app/contact/contact.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  emailAddress: "admin@sweetshoppe.com";
+  businessAddress: "1234 Candyland cir. Denver, CO.";
+  phoneNumber: "111 222 3333";
+  facebookPage: "https://facebook.com/";
 
   constructor() { }
 
