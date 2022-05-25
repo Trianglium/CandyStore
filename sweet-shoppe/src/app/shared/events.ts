4e7ac78d862494cf3100796d70b3a9dddf081017
Jordan <jbrockerrudow@gmail.com>
2022-05-25 05:09:52 -0500

    Begin populating mock database of events

---
+++ sweet-shoppe/src/app/shared/events.ts
@@ -0,0 +1,75 @@
+import { Product } from './product';
+export const PRODUCTS: Product[] = [
+  {
+    id: '0',
+    title: 'Haunted Halloween Hunt 2020',
+    image: '/assets/images/events/haunted-halloween-hunt-2021.jpg',
+    startdate:"2020-10-31T15:00:28.556094Z",
+    enddate:"2020-10-31T17:00:28.556094Z",
+    // tslint:disable-next-line:max-line-length
+    description: 'Families, join our small businesses for a scavenger hunt full of riddles, treats, & spooky fun surprises!',
+  },
+  {
+    id: '1',
+    title: 'Fourth of July Raffle 2021 - Winning Numbers',
+    image: '/assets/images/events/fourth-of-july-raffle-winners-2021.jpg',
+    startdate:"2021-07-03T14:42:28.556094Z",
+    enddate:"2021-07-04T14:30:28.556094Z",
+    // tslint:disable-next-line:max-line-length
+    description: 'Congratulations to all the winners of our 4th of July Raffle! Come on in to Darty’s with your winning ticket to claim your prize of a $10 Darty’s gift card! Reminder: All orders for Candy Grapes are now ready for pickup in store!',
+  },
+  {
+    id: '2,
+    title: 'Fourth of July Raffle 2021',
+    image: '/assets/images/events/fourth-of-july-raffle-2021.jpg',
+    startdate:"2021-06-29T11:30:28.556094Z",
+    enddate:"2021-07-03T14:30:28.556094Z",
+    // tslint:disable-next-line:max-line-length
+    description: 'Make sure to Stop by Darty’s this week to participate in our 4th of July Raffle!',
+  },
+  {
+    id: '3',
+    title: 'Halloween Challenge',
+    image: '/assets/images/events/challengehalloween-10-31-21.jpg',
+    startdate:"2021-10-28T14:30:00.556094Z",
+    enddate:"2021-10-31T17:00:00.556094Z",
+    // tslint:disable-next-line:max-line-length
+    description: 'Come and shop at the Eats & Treats AG pop up shop today. Bring your friends, we will have taste testing, a variety of kid friendly “mock-tails” and an assortment of snacks and goodies!',
+},
+{
+    id: '4',
+    title: 'Halloween Hours',
+    image: '/assets/images/events/halloween-10-31-21.jpg',
+    startdate:"2021-10-31T14:30:00.556094Z",
+    enddate:"2021-10-31T17:00:00.556094Z",
+    // tslint:disable-next-line:max-line-length
+    description: 'Come and shop at the Eats & Treats AG pop up shop today. Bring your friends, we will have taste testing, a variety of kid friendly “mock-tails” and an assortment of snacks and goodies!',
+},
+  {
+    id: '5',
+    title: 'Eats & Treats AG Pop-Up Shop',
+    image: '/assets/images/events/eats-and-treats-11-20-21.jpg',
+    startdate:"2021-10-16T14:00:00.556094Z",
+    enddate:"2021-10-16T18:00:00.556094Z",
+    // tslint:disable-next-line:max-line-length
+    description: 'Come and shop at the Eats & Treats AG pop up shop today. Bring your friends, we will have taste testing, a variety of kid friendly “mock-tails” and an assortment of snacks and goodies!',
+  },
+  {
+    id: '6',
+    title: 'Nacho Night',
+    image: '/assets/images/events/nachonight-12-10-21.jpg',
+    startdate:"2021-12-10T12:30:28.556094Z",
+    enddate:"2021-12-10T18:00:28.556094Z",
+    // tslint:disable-next-line:max-line-length
+    description: 'Join us this Friday, 12/10 for a Nacho Night with @eatsandtreatsbyag!',
+  },
+  {
+    id: '7',
+    title: 'Eats and Treats by AG $30 Valentines Special',
+    image: '/assets/images/events/valentines-day-special-2022.jpg',
+    startdate:"2022-02-04T14:00:00.556094Z",
+    enddate:"2022-02-09T17:00:28.556094Z",
+    // tslint:disable-next-line:max-line-length
+    description: 'Chocolate-covered strawberries is a popular gift this time of the year. If your looking for a source, we got you! Pick-up will be Saturday morning- Sunday afternoon. Give us a call with any questions! (224)888-4496 or send us a message and we\’ll get to you within 6-12hrs. From our hearts to yours, thanks for supporting local! ',
+  },
+]
