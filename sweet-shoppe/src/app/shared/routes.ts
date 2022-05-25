008064095e8cd6f5f726d6291d501d67e75ac1e5
Jordan <jbrockerrudow@gmail.com>
2022-05-25 05:10:07 -0500

    Update Routes const for later integration

--- sweet-shoppe/src/app/shared/routes.ts
+++ sweet-shoppe/src/app/shared/routes.ts
@@ -1,13 +1,42 @@
 import { Routes } from '@angular/router';
-import { MenuComponent } from '../menu/menu.component';
-import { ProductComponent } from '../product/product.component';
-import { HomeComponent } from '../home/home.component';
-import { AboutComponent } from '../about/about.component';
-import { ContactComponent } from '../contact/contact.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProductComponent } from './product/product.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { EventsComponent } from './events/events.component';
 
 export const routes: Routes = [
-  { path: 'home',  component: HomeComponent },
-  { path: 'about',  component: AboutComponent },
-  { path: 'menu',     component: MenuComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'menu', component: MenuComponent,
+    children: [
+      { path: 'product', component: ProductComponent }
+    ]
+  },
+  {
+    path: 'events',
+    component: EventsComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: '**',
+    component: PagenotfoundComponent
+  }
 ];
