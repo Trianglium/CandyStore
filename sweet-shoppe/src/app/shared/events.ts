07101667ba318185b29e9a8fdf19c62da7c1a6d1
Jordan <jbrockerrudow@gmail.com>
2022-05-28 10:47:11 -0500

    Bug Fixes - Syntax errors

--- sweet-shoppe/src/app/shared/events.ts
+++ sweet-shoppe/src/app/shared/events.ts
@@ -1,5 +1,5 @@
-import { Product } from './product';
-export const PRODUCTS: Product[] = [
+import { Event } from './event';
+export const EVENTS: Event[] = [
   {
     id: '0',
     title: 'Haunted Halloween Hunt 2020',
@@ -19,7 +19,7 @@ export const PRODUCTS: Product[] = [
     description: 'Congratulations to all the winners of our 4th of July Raffle! Come on in to Darty’s with your winning ticket to claim your prize of a $10 Darty’s gift card! Reminder: All orders for Candy Grapes are now ready for pickup in store!',
   },
   {
-    id: '2,
+    id: '2',
     title: 'Fourth of July Raffle 2021',
     image: '/assets/images/events/fourth-of-july-raffle-2021.jpg',
     startdate:"2021-06-29T11:30:28.556094Z",
@@ -81,4 +81,4 @@ export const PRODUCTS: Product[] = [
     // tslint:disable-next-line:max-line-length
     description: 'I scream, you scream, we all scream for Ice Cream!! Bring the whole family to Ice Cream Fest 2022! Thanks for supporting local! ',
   }
-]
+];
