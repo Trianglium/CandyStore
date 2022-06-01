582d876e9f83d451aa5f1e80b840aaa5c3f7f236
Jordan <jbrockerrudow@gmail.com>
2022-06-01 10:38:54 -0500

    FlyInOut & Expand animations on home

--- sweet-shoppe/src/app/home/home.component.ts
+++ sweet-shoppe/src/app/home/home.component.ts
@@ -3,11 +3,21 @@ import { Product } from '../shared/product';
 import { ProductService } from '../services/product.service';
 import { Event } from '../shared/event';
 import { EventService } from '../services/event.service';
+import { flyInOut, expand } from '../animations/app.animation';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  // tslint:disable-next-line:use-host-property-decorator
+  host: {
+  '[@flyInOut]': 'true',
+  'style': 'display: block;'
+  },
+  animations: [
+    flyInOut(),
+    expand()
+  ]
 })
 export class HomeComponent implements OnInit {
   product: Product;
