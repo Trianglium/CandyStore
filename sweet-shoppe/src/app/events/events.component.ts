2939f5b843e8971aa72b0d7dce8885e3ce26c2ea
Jordan <jbrockerrudow@gmail.com>
2022-06-01 10:39:33 -0500

    FlyInOut Route change suupport added

--- sweet-shoppe/src/app/events/events.component.ts
+++ sweet-shoppe/src/app/events/events.component.ts
@@ -1,9 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { flyInOut, expand } from '../animations/app.animation';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.scss']
+  styleUrls: ['./events.component.scss'],
+  // tslint:disable-next-line:use-host-property-decorator
+  host: {
+  '[@flyInOut]': 'true',
+  'style': 'display: block;'
+  },
+  animations: [
+    flyInOut(),
+    expand()
+  ]
 })
 export class EventsComponent implements OnInit {
 
