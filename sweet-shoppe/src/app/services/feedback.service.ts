c3eeaac10955b4afebde1e8796d170f4a87f787d
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-06-01 12:47:01 -0500

    HTTP Client integrated into feedback service

--- sweet-shoppe/src/app/services/feedback.service.ts
+++ sweet-shoppe/src/app/services/feedback.service.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@angular/core';
-import { Feedback, ContactType } from '../shared/feedback';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Feedback } from '../shared/feedback';
+
+import { baseURL } from '../shared/baseurl';
+import { catchError } from 'rxjs/operators';
+import { ProcessHTTPMsgService } from './process-httpmsg.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedbackService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private processHTTPMsgService: ProcessHTTPMsgService) { }
 
-  getFeedback(): Feedback[] {
-    return;
+  submitFeedback(feedback: Feedback): Observable<Feedback> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    return this.http.post<Feedback>(baseURL + 'feedback', feedback, httpOptions)
+      .pipe(catchError(this.processHTTPMsgService.handleError));
   }
-  OnSubmit() {
 
-  }
 }
