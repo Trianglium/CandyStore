e388728379c960c975788116a7a80b9692e752c5
Jordan <jbrockerrudow@gmail.com>
2022-06-01 10:41:50 -0500

    Animation to render view from fetched data

--- sweet-shoppe/src/app/animations/app.animation.ts
+++ sweet-shoppe/src/app/animations/app.animation.ts
@@ -27,3 +27,13 @@ export function flyInOut() {
         ])
     ]);
 }
+// Animation to Render View from Fetched Data
+export function expand() {
+    return trigger('expand', [
+        state('*', style({ opacity: 1, transform: 'translateX(0)' })),
+        transition(':enter', [
+            style({ transform: 'translateY(-50%)', opacity:0 }),
+            animate('200ms ease-in', style({ opacity: 1, transform: 'translateX(0)' }))
+        ])
+    ]);
+}
