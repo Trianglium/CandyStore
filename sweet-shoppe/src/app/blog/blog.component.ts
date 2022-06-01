615dfb481b647034dc22dbad7d57213b96c3851b
Jordan <jbrockerrudow@gmail.com>
2022-06-01 10:39:57 -0500

    Animations for route support & transiition integrated

--- sweet-shoppe/src/app/blog/blog.component.ts
+++ sweet-shoppe/src/app/blog/blog.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { flyInOut } from '../animations/app.animation';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.scss']
+  styleUrls: ['./blog.component.scss'],
+  // tslint:disable-next-line:use-host-property-decorator
+  host: {
+  '[@flyInOut]': 'true',
+  'style': 'display: block;'
+  },
+  animations: [
+    flyInOut()
+  ]
 })
 export class BlogComponent implements OnInit {
 
