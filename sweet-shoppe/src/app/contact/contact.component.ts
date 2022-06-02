906e664219d167a522cfeb0f2deaad7757562e83
Jordan <jbrockerrudow@gmail.com>
2022-06-02 11:12:48 -0500

    Feedback form validation, error handling, implementation

--- sweet-shoppe/src/app/contact/contact.component.ts
+++ sweet-shoppe/src/app/contact/contact.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, NgForm } from '@angular/forms';
+
 import { Feedback, ContactType } from '../shared/feedback';
-import { flyInOut } from '../animations/app.animation';
+import { flyInOut, expand } from '../animations/app.animation';
+import { FeedbackService } from '../services/feedback.service';
 
 @Component({
   selector: 'app-contact',
@@ -13,28 +15,113 @@ import { flyInOut } from '../animations/app.animation';
   'style': 'display: block;'
   },
   animations: [
-    flyInOut()
+    flyInOut(),
+    expand()
   ]
 })
 export class ContactComponent implements OnInit {
-  emailAddress: "admin@sweetshoppe.com";
-  businessAddress: "1234 Candyland cir. Denver, CO.";
-  phoneNumber: "111 222 3333";
-  facebookPage: "https://facebook.com/";
-
   feedbackForm: FormGroup;
   feedback: Feedback;
-  contactType = ContactType;
+  feedbackCopy: any = Feedback;
+  contactType: any = ContactType;
+  errMess: string;
+  submitted: boolean = true;
+  displaySpinner: boolean = false;
+  details: boolean;
+  @ViewChild('fform') feedbackFormDirective: NgForm;
 
-  constructor(private fb: FormBuilder) {
+  formErrors: any = {
+    'firstname': '',
+    'lastname': '',
+    'phonenum': '',
+    'email': ''
+  };
+
+  validationMessages: any = {
+    'firstname': {
+      'required':      'First Name is required.',
+      'minlength':     'First Name must be at least 2 characters long.',
+      'maxlength':     'FirstName cannot be more than 25 characters long.'
+    },
+    'lastname': {
+      'required':      'Last Name is required.',
+      'minlength':     'Last Name must be at least 2 characters long.',
+      'maxlength':     'Last Name cannot be more than 25 characters long.'
+    },
+    'phonenum': {
+      'required':      'Tel. number is required.',
+      'pattern':       'Tel. number must contain only numbers.'
+    },
+    'email': {
+      'required':      'Email is required.',
+      'email':         'Email not in valid format.'
+    },
+  };
+
+  constructor(private fb: FormBuilder, private feedbackService: FeedbackService) {
     this.createForm();
   }
 
   ngOnInit(): void {
+    this.submitted = false;
   }
 
-  createForm() {
+  createForm(): void {
     this.feedbackForm = this.fb.group({
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      phonenum: [0, [Validators.required, Validators.pattern] ],
+      email: ['', [Validators.required, Validators.email] ],
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+    this.feedbackForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+    this.onValueChanged(); // set or reset validation message
+  }
+
+
+  onValueChanged(data?: any) {
+    if (!this.feedbackForm) { return; }
+    const form = this.feedbackForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    this.feedback = this.feedbackForm.value;
+    console.log(this.feedback);
+    this.displaySpinner = true;
+    this.feedbackService.submitFeedback(this.feedback).subscribe((response) => {
+    console.log(response);
+    this.feedbackCopy = response;
+      this.displaySpinner = false;
+      this.details = true;
+      setTimeout(() => {
+        this.submitted = false;
+        this.details = false;
+
+      }, 5000);
+
+    });
+
+    this.feedbackForm.reset({
       firstname: '',
       lastname: '',
       phonenum: 0,
@@ -43,13 +130,7 @@ export class ContactComponent implements OnInit {
       contacttype: 'None',
       message: ''
     });
+    this.feedbackFormDirective.resetForm();
   }
-
-  onSubmit() {
-    this.feedback = this.feedbackForm.value;
-    console.log(this.feedback);
-    this.feedbackForm.reset();
-  }
-
 
 }
