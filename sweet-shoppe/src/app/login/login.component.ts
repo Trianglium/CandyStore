ee89dcb07528c30c745371243d907ca42df5b6f6
Jordan <jbrockerrudow@gmail.com>
2022-06-02 11:23:37 -0500

    Implement onSubmit login function & form fields

--- sweet-shoppe/src/app/login/login.component.ts
+++ sweet-shoppe/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  user = {username: '', password: '', remember: false};
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<LoginComponent>) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log('User: ', this.user);
+    this.dialogRef.close();
   }
 
 }
