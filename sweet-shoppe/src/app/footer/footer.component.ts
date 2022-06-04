37b482c56b73b746c8dfbff56c06b1c7a21c7222
Jordan <jbrockerrudow@gmail.com>
2022-06-04 11:34:46 -0500

    Re implmentation of food - styles integrated

--- sweet-shoppe/src/app/footer/footer.component.ts
+++ sweet-shoppe/src/app/footer/footer.component.ts
@@ -6,10 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  emailAddress: "admin@sweetshoppe.com";
-  businessAddress: "1234 Candyland cir. Denver, CO.";
-  phoneNumber: "111 222 3333";
-  facebookPage: "https://facebook.com/";
 
   constructor() { }
 
