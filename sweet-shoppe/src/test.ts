4ab316c4915ecbde8226efbafbc09304f95095a1
Jordan <jbrockerrudow@gmail.com>
2022-06-04 11:45:34 -0500

    Version2 app implemented

--- sweet-shoppe/src/test.ts
+++ sweet-shoppe/src/test.ts
@@ -21,6 +21,11 @@ getTestBed().initTestEnvironment(
 );
 
 // Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
+//const context = require.context('./', true, /\.spec\.ts$/);
+
+// Enables ability to run a single test on a specific component rather than all the tests.
+const context = require.context('./', true, /menu\.component\.spec\.ts$/);
+
+
 // And load the modules.
 context.keys().map(context);
