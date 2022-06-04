7beb3646cf61f4f447bdfee3476dc6d2ed94a0f7
Jordan <jbrockerrudow@gmail.com>
2022-06-04 11:32:19 -0500

    Promo Service, Model Schema & Data

--- sweet-shoppe/src/app/services/promotion.service.ts
+++ sweet-shoppe/src/app/services/promotion.service.ts
@@ -21,12 +21,12 @@ export class PromotionService {
       return this.http.get<Promotion[]>(baseURL + 'promotions')
       .pipe(catchError(this.processHTTPMsgService.handleError));
     }
-
+  
     getPromotion(id: string): Observable<Promotion> {
       return this.http.get<Promotion>(baseURL + 'promotions/' + id)
       .pipe(catchError(this.processHTTPMsgService.handleError));
     }
-
+  
     getFeaturedPromotion(): Observable<Promotion> {
       return this.http.get<Promotion[]>(baseURL + 'promotions?featured=true').pipe(map(promotions => promotions[0]))
       .pipe(catchError(this.processHTTPMsgService.handleError));
---
+++ sweet-shoppe/src/app/shared/promotions.ts
@@ -0,0 +1,13 @@
+import { Promotion } from './promotion';
+
+export const PROMOTIONS: Promotion[] = [
+    {
+      id: '0',
+      name: 'Ice Cream Fest 2022',
+      image: '/assets/images/events/iceCream-fest-brick-2022.png',
+      label: 'New',
+      featured: true,
+      // tslint:disable-next-line:max-line-length
+      description: 'As the weathers getting colder, things are only starting to heat up at Darty/’s! Think you/’ve got what it takes to own the One chip challenge? Send us your video submissions via Instagram or Facebook to be featured on our page!'
+    }
+];
