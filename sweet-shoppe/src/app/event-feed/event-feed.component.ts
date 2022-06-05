ba6a440bdc85b47a8b010e234f01e3478b8cf11b
Jordan <jbrockerrudow@gmail.com>
2022-06-05 09:26:48 -0500

    Event service integrated

--- sweet-shoppe/src/app/event-feed/event-feed.component.ts
+++ sweet-shoppe/src/app/event-feed/event-feed.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { Event } from '../shared/event';
+import { EventService } from '../services/event.service';
+import { flyInOut } from '../animations/app.animation';
 
 @Component({
   selector: 'app-event-feed',
   templateUrl: './event-feed.component.html',
-  styleUrls: ['./event-feed.component.scss']
+  styleUrls: ['./event-feed.component.scss'],
+  // tslint:disable-next-line:use-host-property-decorator
+  host: {
+  '[@flyInOut]': 'true',
+  'style': 'display: block;'
+  },
+  animations: [
+    flyInOut()
+  ]
 })
 export class EventFeedComponent implements OnInit {
 
-  constructor() { }
+  events: Event[];
+  errMess: string;
+
+
+  constructor(
+    private eventService: EventService,
+    @Inject('BaseURL') public BaseURL: any
+  ) { }
 
   ngOnInit(): void {
+    this.eventService.getEvents()
+     .subscribe(events => this.events = events,
+       errmess => this.errMess = <any>errmess);
   }
 
 }
